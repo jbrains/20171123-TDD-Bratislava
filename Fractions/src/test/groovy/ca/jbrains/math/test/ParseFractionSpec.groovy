a4d2a350713f598abd0b5c40ec10fb3f63bd4fc8
J. B. Rainsberger <me@jbrains.ca>
2017-11-23 12:41:40 +0100

    We can parse any text as a Fraction, as long as it's 0.

---
+++ Fractions/src/test/groovy/ca/jbrains/math/test/ParseFractionSpec.groovy
@@ -0,0 +1,18 @@
+package ca.jbrains.math.test
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+
+class ParseFractionSpec extends Specification {
+    @Unroll
+    def "#text parses as #fraction"() {
+        expect:
+        Fraction.parse(text) == fraction
+
+        where:
+        text | fraction
+        "0" | new Fraction(0)
+    }
+
+}
---
+++ Fractions/src/test/java/ca/jbrains/math/test/Fraction.java
@@ -0,0 +1,48 @@
+package ca.jbrains.math.test;
+
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(final int intValue) {
+        this(intValue, 1);
+    }
+
+    public Fraction(final int numerator, final int denominator) {
+        if (denominator == 0)
+            throw new IllegalArgumentException("A Fraction cannot have a zero denominator.");
+
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public static Fraction parse(final String text) {
+        return new Fraction(0);
+    }
+
+    public Fraction plus(final Fraction that) {
+        return new Fraction(
+                this.numerator * that.denominator + that.numerator * this.denominator,
+                this.denominator * that.denominator);
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        if (other instanceof Fraction) {
+            Fraction that = (Fraction) other;
+            return this.numerator * that.denominator == this.denominator * that.numerator;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return numerator * 137 + denominator * 67;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d/%d", numerator, denominator);
+    }
+}
