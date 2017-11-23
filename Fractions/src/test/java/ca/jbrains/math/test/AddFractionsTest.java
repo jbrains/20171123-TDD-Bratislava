bbb3cffa34a5b4f0aae33d2620ef589341d867d5
J. B. Rainsberger <me@jbrains.ca>
2017-11-23 11:32:07 +0100

    We can add any fractions as long as the second one is 0.

--- Fractions/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ Fractions/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -1,23 +1,38 @@
 package ca.jbrains.math.test;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AddFractionsTest {
     @Test
     public void zeroPlusZero() throws Exception {
-        final Fraction zero = new Fraction();
+        final Fraction zero = new Fraction(0);
         Fraction sum = zero.plus(zero);
         Assert.assertEquals(0, sum.intValue());
     }
 
+    @Test
+    public void notZeroPlusZero() throws Exception {
+        final Fraction zero = new Fraction(0);
+        final Fraction six = new Fraction(6);
+        Fraction sum = six.plus(zero);
+        Assert.assertEquals(6, sum.intValue());
+    }
+
     public static class Fraction {
+        private final int intValue;
+
+        public Fraction(final int intValue) {
+            this.intValue = intValue;
+        }
+
         public Fraction plus(final Fraction zero) {
             return this;
         }
 
         public int intValue() {
-            return 0;
+            return intValue;
         }
     }
 }
