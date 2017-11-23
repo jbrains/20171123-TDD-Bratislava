443c8955796d15d18bc510d7bb83b3b579560e7b
J. B. Rainsberger <me@jbrains.ca>
2017-11-23 15:09:10 +0100

    We can sell different items, but the default price seems to be €1.87.

--- PointOfSale/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ PointOfSale/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -6,22 +6,48 @@ import org.junit.Test;
 public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
-        final Sale sale = new Sale();
         final Display display = new Display();
+        final Sale sale = new Sale(display);
 
         sale.onBarcode("1234");
 
         Assert.assertEquals("€ 5.50", display.getText());
     }
 
-    public static class Sale {
-        public void onBarcode(final String barcode) {
+    @Test
+    public void anotherProductFound() throws Exception {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
 
+        sale.onBarcode("2345");
+
+        Assert.assertEquals("€ 1.87", display.getText());
+    }
+
+    public static class Sale {
+        private final Display display;
+
+        public Sale(final Display display) {
+            this.display = display;
+        }
+
+        public void onBarcode(final String barcode) {
+            if ("1234".equals(barcode))
+                display.setText("€ 5.50");
+            else
+                display.setText("€ 1.87");
         }
     }
+
     public static class Display {
+        private String text;
+
         public String getText() {
-            return "€ 5.50";
+            return text;
+        }
+
+        public void setText(final String text) {
+            this.text = text;
         }
     }
 }
