40399023d4c9f14c3a3139b365c3471c10163ca4
J. B. Rainsberger <me@jbrains.ca>
2017-11-23 12:56:43 +0100

    We refuse to parse fractions that represent invalid values.

--- Fractions/src/test/groovy/ca/jbrains/math/test/ParseFractionSpec.groovy
+++ Fractions/src/test/groovy/ca/jbrains/math/test/ParseFractionSpec.groovy
@@ -11,13 +11,13 @@ class ParseFractionSpec extends Specification {
         Fraction.parse(text) == fraction
 
         where:
-        text | fraction
-        "0" | new Fraction(0)
-        "5" | new Fraction(5)
-        "-2" | new Fraction(-2)
-        "1/2" | new Fraction(1, 2)
+        text    | fraction
+        "0"     | new Fraction(0)
+        "5"     | new Fraction(5)
+        "-2"    | new Fraction(-2)
+        "1/2"   | new Fraction(1, 2)
         "15/25" | new Fraction(3, 5)
-        "5/-3" | new Fraction(-5, 3)
+        "5/-3"  | new Fraction(-5, 3)
     }
 
     @Unroll
@@ -30,6 +30,18 @@ class ParseFractionSpec extends Specification {
 
         where:
         text << ["", " ", "\t", "/", "1/", "/8", "a/b", "a/6", "7/b", "abc"]
+    }
+
+    @Unroll
+    def "\"#text\" parses as an invalid fraction"() {
+        when:
+        Fraction.parse(text)
+
+        then:
+        thrown(IllegalArgumentException)
+
+        where:
+        text << ["1/0", "-345/0", "91237/0", "0/0"]
     }
 
 }
