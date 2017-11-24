1762d45b3ce2ee5ca10870579d01aefb1de619f1
J. B. Rainsberger <me@jbrains.ca>
2017-11-24 12:53:11 +0100

    Clarified the intent of tests.

--- PointOfSale/src/test/java/ca/jbrains/learn/poi/test/RoundTripWorkbookTest.java
+++ PointOfSale/src/test/java/ca/jbrains/learn/poi/test/RoundTripWorkbookTest.java
@@ -7,20 +7,28 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.junit.Assert;
 import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.IOException;
 import java.util.Iterator;
 
+import static org.hamcrest.Matchers.is;
+
 public class RoundTripWorkbookTest {
+
+    private File simpleWorkbookFile;
+
+    @Before
+    public void workbook() throws Exception {
+        simpleWorkbookFile = new File("./src/test/data/simple-workbook.xls");
+        Assume.assumeTrue(simpleWorkbookFile.exists());
+    }
+
     @Test
     public void workbookStructure() throws Exception {
-        final File simpleWorkbookFile = new File("./src/test/data/simple-workbook.xls");
-        Assume.assumeTrue(simpleWorkbookFile.exists());
-
         final HSSFWorkbook simpleWorkbook = new HSSFWorkbook(new FileInputStream(simpleWorkbookFile));
         Assert.assertEquals(1, simpleWorkbook.getNumberOfSheets());
 
@@ -32,14 +40,29 @@ public class RoundTripWorkbookTest {
 
         final Iterator<Cell> cellsInFirstRow = headerRow.cellIterator();
         Assert.assertTrue(cellsInFirstRow.hasNext());
+    }
 
-        final Cell cellA1 = cellsInFirstRow.next();
-        Assert.assertEquals("strings", cellA1.getStringCellValue());
+    @Test
+    public void readHeaderRowCellValuesAsText() throws Exception {
+        final HSSFRow headerRow = sanityCheckForWorkbook();
 
         final List<Cell> cellsInFirstRowAsList = List.ofAll(() -> headerRow.cellIterator());
-        Assert.assertEquals(3, cellsInFirstRowAsList.size());
+        final List<String> cellValuesInFirstRowAsText = cellsInFirstRowAsList.map(Cell::getStringCellValue);
+
         Assert.assertEquals(
                 List.of("strings", "money amounts", "plain numbers"),
-                cellsInFirstRowAsList.map(Cell::getStringCellValue));
+                cellValuesInFirstRowAsText);
+    }
+
+    private HSSFRow sanityCheckForWorkbook() throws IOException {
+        final HSSFWorkbook simpleWorkbook = new HSSFWorkbook(new FileInputStream(simpleWorkbookFile));
+        Assume.assumeThat(simpleWorkbook.getNumberOfSheets(), is(1));
+
+        final HSSFSheet sampleDataWorksheet = simpleWorkbook.getSheet("Sample Data");
+        Assume.assumeNotNull(sampleDataWorksheet);
+
+        final HSSFRow headerRow = sampleDataWorksheet.getRow(0);
+        Assume.assumeNotNull(headerRow);
+        return headerRow;
     }
 }
