21222eaa08ff6d80a44a5ae39b5f2394b2e92519
J. B. Rainsberger <me@jbrains.ca>
2017-11-24 14:56:34 +0100

    We now dispatch commands in a way that allows for any number of commands.

--- PointOfSale/src/test/java/ca/jbrains/pos/ui/test/CommandLineInterfaceTest.java
+++ PointOfSale/src/test/java/ca/jbrains/pos/ui/test/CommandLineInterfaceTest.java
@@ -1,7 +1,9 @@
 package ca.jbrains.pos.ui.test;
 
+import io.vavr.collection.List;
 import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -33,10 +35,22 @@ public class CommandLineInterfaceTest {
         process(new StringReader("::barcode::"));
     }
 
+    @Test
+    @Ignore
+    public void severalCommands() throws Exception {
+        context.checking(new Expectations() {{
+            oneOf(barcodeScannedListener).onBarcode("::barcode 1::");
+            oneOf(barcodeScannedListener).onBarcode("::barcode 2::");
+            oneOf(barcodeScannedListener).onBarcode("::barcode 3::");
+        }});
+
+        process(new StringReader("::barcode 1::\n::barcode 2::\n::barcode 3::"));
+    }
+
     private void process(final Reader commandSource) throws IOException {
         final String theLine = new BufferedReader(commandSource).readLine();
-        if (theLine != null)
-            barcodeScannedListener.onBarcode(theLine);
+        List<String> lines = (theLine == null) ? List.empty() : List.of(theLine);
+        lines.forEach(barcodeScannedListener::onBarcode);
     }
 
     public interface BarcodeScannedListener {
