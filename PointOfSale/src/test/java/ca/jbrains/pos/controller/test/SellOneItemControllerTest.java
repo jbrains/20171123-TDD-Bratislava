6d76f6cba3f987f5018815aa0eed1cefcb80838d
J. B. Rainsberger <me@jbrains.ca>
2017-11-24 11:46:18 +0100

    We can find a price in memory, as long as we have the price.

--- PointOfSale/src/test/java/ca/jbrains/pos/controller/test/SellOneItemControllerTest.java
+++ PointOfSale/src/test/java/ca/jbrains/pos/controller/test/SellOneItemControllerTest.java
@@ -65,17 +65,6 @@ public class SellOneItemControllerTest {
         void displayEmptyBarcodeMessage();
     }
 
-    public static class Price {
-        public static Price cents(final int centsValue) {
-            return new Price();
-        }
-
-        @Override
-        public String toString() {
-            return "a Price";
-        }
-    }
-
     public static class SellOneItemController {
         private final Catalog catalog;
         private final Display display;
---
+++ PointOfSale/src/test/java/ca/jbrains/pos/model/test/InMemoryCatalogTest.java
@@ -0,0 +1,31 @@
+package ca.jbrains.pos.model.test;
+
+import ca.jbrains.pos.controller.test.Price;
+import io.vavr.collection.HashMap;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class InMemoryCatalogTest {
+    @Test
+    public void productFound() throws Exception {
+        final Price matchingPrice = Price.cents(1250);
+        final InMemoryCatalog catalog = new InMemoryCatalog(
+                HashMap.of("12345", matchingPrice).toJavaMap());
+
+        Assert.assertEquals(matchingPrice, catalog.findPrice("12345"));
+    }
+
+    public static class InMemoryCatalog {
+        private final Map<String, Price> pricesByBarcode;
+
+        public InMemoryCatalog(final Map<String, Price> pricesByBarcode) {
+            this.pricesByBarcode = pricesByBarcode;
+        }
+
+        public Price findPrice(final String barcode) {
+            return pricesByBarcode.get(barcode);
+        }
+    }
+}
